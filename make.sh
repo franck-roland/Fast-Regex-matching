f3c1a2deb81bb9aa659b1b8216ff2b9547858769
Franck Roland <franck.l.roland@gmail.com>
2012-05-25 21:17:42 +0200

    modules splitted

--- make.sh
+++ make.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 gcc -pthread -Wall -fno-strict-aliasing -DNDEBUG -g -fwrapv -O2 -Wall -Wstrict-prototypes -fPIC -I/usr/include/python2.7 -c libRegex.c -o libRegex.o;
 gcc -pthread -Wall -shared -Wl,-O1 -Wl,-Bsymbolic-functions -Wl,-z,relro libRegex.o -o _libRegex.so
+#-L/usr/lib/python2.7/config -lpthread -ldl -lutil -lm -lpython2.7 -Xlinker -export-dynamic -Wl,-O1 -Wl,-Bsymbolic-functions
